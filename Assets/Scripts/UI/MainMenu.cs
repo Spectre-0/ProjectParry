147b841885d3396e69c6a041cb49cb668c58a392
Al Adnan Uddin <shuhanuddin6@gmail.com>
2023-10-31 22:35:11 +0000

    updated main menu
    added options menu and keybind menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenuPanel;      // Assign the MainMenu panel from the inspector
+    public GameObject optionsMenuPanel;   // Assign the OptionsMenu panel from the inspector
+    public GameObject keyBindMenuPanel;   // Assign the KeyBindMenu panel from the inspector
+
+    private void Start()
+    {
+        ShowMainMenu();
+    }
 
     public void PlayGame()
     {
@@ -15,5 +23,36 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        optionsMenuPanel.SetActive(false);
+        keyBindMenuPanel.SetActive(false);
+    }
+
+    public void ShowOptionsMenu()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(true);
+        keyBindMenuPanel.SetActive(false);
+    }
+
+    public void ShowKeyBindMenu()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        keyBindMenuPanel.SetActive(true);
+    }
+
+    public void BackFromKeyBindsToOptions()
+    {
+        ShowOptionsMenu();
+    }
+
+    public void BackFromOptionsToMainMenu()
+    {
+        ShowMainMenu();
     }
 }
